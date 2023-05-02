6124db833017ca91902983d3d82e9c9b1d7aa9de
wadiididah <60482888+WadiiDidah@users.noreply.github.com>
2023-05-02 18:27:24 +0200

    Modification des test de IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,4 +1,4 @@
-package fr.univavignon.pokedex.api;
+ 	package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -14,8 +14,8 @@ import org.mockito.MockitoAnnotations;
 public class IPokedexFactoryTest {
 	@Mock
 	private IPokemonMetadataProvider pokemonMetadataProvider;
-	@Mock
-	private IPokedexFactory ipokedexFactory;
+	
+	private IPokedexFactory pokedexFactory;
 	@Mock
 	private IPokemonFactory factory;
 	@Mock
@@ -24,10 +24,9 @@ public class IPokedexFactoryTest {
 	@Before
 	public void initialiser() throws PokedexException {
 		MockitoAnnotations.initMocks(this);
-		PokemonMetadata pokemonMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
-		when(pokemonMetadataProvider.getPokemonMetadata(anyInt())).thenAnswer(input -> {
-			return pokemonMetadata;
-		});
+		 pokedexFactory = new PokedexFactory();
+		
+		
 
 	}
 
@@ -35,17 +34,11 @@ public class IPokedexFactoryTest {
 	public void Test_GetPokemon() throws PokedexException {
 		Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 614, 64, 4000, 4, 56);
 		iPokedex.addPokemon(pokemon);
-		when(ipokedexFactory.createPokedex(pokemonMetadataProvider, factory)).thenReturn(iPokedex);
-
-		when(factory.createPokemon(anyInt(), eq(613), eq(64), eq(4000), eq(4))).thenAnswer(input -> {
-
-			return pokemon;
-
-		});
 		
-		assertEquals(ipokedexFactory.createPokedex(pokemonMetadataProvider, factory), iPokedex);
-		assertEquals(ipokedexFactory.createPokedex(pokemonMetadataProvider, factory).getPokemon(0), iPokedex.getPokemon(0));
 		
+		
+        assertEquals(Pokedex.class,pokedexFactory.createPokedex(pokemonMetadataProvider,factory).getClass());
+
 
 	}
 
